beca6b37c40f67bcccd66cfbd60ba4fd0dd0c70c
gongthub <1056685839@qq.com>
2017-01-10 17:46:59 +0800

    添加上传图片

--- Code/CMS/CMS.Web/Areas/WebManage/Controllers/C_ContentController.cs
+++ Code/CMS/CMS.Web/Areas/WebManage/Controllers/C_ContentController.cs
@@ -3,8 +3,9 @@ using CMS.Code;
 using CMS.Domain.Entity.WebManage;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Web.Mvc; 
+using System.Web.Mvc;
 
 namespace CMS.Web.Areas.WebManage.Controllers
 {
@@ -19,7 +20,7 @@ namespace CMS.Web.Areas.WebManage.Controllers
         {
             var data = c_contentApp.GetList(itemId, keyword);
             return Content(data.ToJson());
-        } 
+        }
         [HttpGet]
         [HandlerAjaxOnly]
         public ActionResult GetFormJson(string keyValue)
@@ -32,8 +33,39 @@ namespace CMS.Web.Areas.WebManage.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult SubmitForm(C_ContentEntity c_ContentEntity, string keyValue)
         {
-            c_contentApp.SubmitForm(c_ContentEntity, keyValue);
-            return Success("操作成功。");
+            try
+            {
+                string upPaths = "~/Uploads/";
+                if (HttpContext.Request.Files.Count > 0 && HttpContext.Request.Files["F_Icon"] != null)
+                {
+                    var iconFile = HttpContext.Request.Files["F_Icon"];
+                    if (iconFile != null)
+                    {
+                        // 文件上传后的保存路径
+                        string filePath = Server.MapPath(upPaths);
+                        if (!Directory.Exists(filePath))
+                        {
+                            Directory.CreateDirectory(filePath);
+                        }
+                        string fileName = Path.GetFileName(iconFile.FileName);// 原始文件名称
+                        string fileExtension = Path.GetExtension(fileName); // 文件扩展名
+                        Random random = new Random();
+                        string randomStr = random.Next(0000, 9999).ToString();
+                        string saveName = DateTime.Now.ToString("yyyyMMddHHmmss") + randomStr + fileExtension; // 保存文件名称
+                        string filePaths = upPaths + saveName;
+                        iconFile.SaveAs(filePaths);
+                        c_ContentEntity.F_Icon = filePaths;
+                    }
+
+                }
+                c_contentApp.SubmitForm(c_ContentEntity, keyValue);
+                return Success("操作成功。");
+
+            }
+            catch (Exception ex)
+            {
+                return Error("操作失败。" + ex.Message);
+            }
         }
         [HttpPost]
         [HandlerAjaxOnly]
@@ -50,9 +82,51 @@ namespace CMS.Web.Areas.WebManage.Controllers
         [HandlerAuthorize]
         [ValidateAntiForgeryToken]
         public ActionResult GetStaticPage(string keyValue)
-        {  
-            c_contentApp.GetStaticPage(keyValue); 
+        {
+            c_contentApp.GetStaticPage(keyValue);
             return Success("生成成功。");
+        }
+
+
+        //[ValidateAntiForgeryToken]
+        public ActionResult UploadImg()
+        {
+            try
+            {
+                string upPaths = "~/Uploads/";
+                string upPathsT = "/Uploads/";
+                string filePaths = string.Empty;
+                if (HttpContext.Request.Files.Count > 0)
+                {
+                    var upFiles = HttpContext.Request.Files[0];
+                    if (upFiles != null)
+                    {
+                        // 文件上传后的保存路径
+                        string filePath = Server.MapPath(upPaths);
+                        if (!Directory.Exists(filePath))
+                        {
+                            Directory.CreateDirectory(filePath);
+                        }
+                        string fileName = Path.GetFileName(upFiles.FileName);// 原始文件名称
+                        string fileExtension = Path.GetExtension(fileName); // 文件扩展名
+                        Random random = new Random();
+                        string randomStr = random.Next(0000, 9999).ToString();
+                        string saveName = DateTime.Now.ToString("yyyyMMddHHmmss") + randomStr + fileExtension; // 保存文件名称
+                        filePaths = upPathsT + saveName; 
+                        upFiles.SaveAs(filePath + saveName);
+                    }
+                }
+                else
+                {
+                    return Success("true");
+                }
+                return Success("true", filePaths);
+
+            }
+            catch (Exception ex)
+            {
+                return Success("false",ex.Message);
+            }
         }
     }
 }
