5fec79fb9d260af796fe7bcffd8db3e290e90893
gongthub <1056685839@qq.com>
2017-01-16 17:16:24 +0800

    优化分页

--- Code/CMS/CMS.Web/Content/js/TabPage/cmsPage.js
+++ Code/CMS/CMS.Web/Content/js/TabPage/cmsPage.js
@@ -8,20 +8,14 @@ $(function () {
     }
 
 });
-////每页条数
-//var PAGESIZE = 10;
-////显示类型
-//var PAGETYPE = "";
-////所有信息条数
-//var INFOSUM = 0;
-////总页数
-//var PAGENUM = 0;
+//初始化属性
 function initPageAttr(e) {
     //传递对象
     var pageobj = {
         obj: e,
         pagesize: 10,
         pagetype: "",
+        pagestyle: "onlynum",
         infosum: 0,
         pagenum: 0,
         selectindex: 1,
@@ -29,19 +23,24 @@ function initPageAttr(e) {
 
     var infoType = $(e).attr("info-type");
     var infoPageSize = $(e).attr("info-pagesize");
+    var pageStyle = $(e).attr("page-style");
+
     if (infoType != null && infoType != undefined && infoType.trim() != "") {
         pageobj.pagetype = infoType;
         if (infoPageSize != null && infoPageSize != undefined && infoPageSize.trim() != "") {
             if (!isNaN(infoPageSize)) {
                 pageobj.pagesize = infoPageSize;
-                initPage(pageobj);
             } else {
                 alert("info-pagesize必须是数字！");
             }
         }
+        if (pageStyle != null && pageStyle != undefined && pageStyle.trim() != "") {
+            pageobj.pagestyle = pageStyle;
+        }
+        initPage(pageobj);
     }
 }
-
+//分页处理
 function initPage(pageobj) {
     var pageInfos = $(pageobj.obj).find(pageobj.pagetype);
     if (pageInfos != null && pageInfos != undefined && pageInfos.length > 0) {
@@ -59,6 +58,8 @@ function initPage(pageobj) {
         initPageTab(pageobj);
     }
 }
+
+//分页控件处理
 function initPageTab(pageobj) {
 
     var pagectl = $(pageobj.obj).find(".div-cmspage-pagetab");
@@ -71,8 +72,12 @@ function initPageTab(pageobj) {
         for (var i = 1; i <= pageobj.pagenum; i++) {
             var pageNumber = document.createElement("span");
             pageNumber.innerText = i;
+            if (pageobj.selectindex == i) {
+                pageNumber.className = "div-cmspage-pagetab-ctl-active";
+            } else {
+                pageNumber.className = "div-cmspage-pagetab-ctl";
+            }
             pageNumber.onclick = function (e) {
-                //alert(e.toElement.innerText);
                 pageobj.selectindex = e.toElement.innerText;
                 pageChange(pageobj)
             };
@@ -80,9 +85,45 @@ function initPageTab(pageobj) {
         }
     }
     pageobj.obj.appendChild(pageControl);
+    initPageCtlStyle(pageobj);
 
 }
 
-function pageChange(pageobj) { 
+/* 
+onlynum:仅有数字 
+haspn:包含上一页下一页
+
+*/
+//处理分页控件样式
+function initPageCtlStyle(pageobj) {
+    if (pageobj.pagestyle == "haspn") {
+        var pagectl = $(pageobj.obj).find(".div-cmspage-pagetab");
+        if (pagectl[0] != null && pagectl[0] != undefined) {
+            var pageNumberNext = document.createElement("span");
+            pageNumberNext.innerText = "下一页";
+            pageNumberNext.onclick = function (e) { 
+                if (pageobj.selectindex < pageobj.pagenum) {
+                    pageobj.selectindex = Number(pageobj.selectindex)+1;
+                    pageChange(pageobj)
+                }
+            };
+            pagectl[0].appendChild(pageNumberNext);
+
+
+            var pageNumberPre = document.createElement("span");
+            pageNumberPre.innerText = "上一页";
+            pageNumberPre.onclick = function (e) { 
+                if (pageobj.selectindex > 1) {
+                    pageobj.selectindex = Number(pageobj.selectindex) - 1;
+                    pageChange(pageobj)
+                }
+            };
+            pagectl[0].prepend(pageNumberPre);
+        }
+    }
+}
+ 
+//换页事件
+function pageChange(pageobj) {
     initPage(pageobj);
 }
